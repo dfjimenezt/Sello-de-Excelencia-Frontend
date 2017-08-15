d3b4df9f4b920e1130dcb90181b297e87b31c583
Daniel Jiménez <daniel@domoti.rocks>
2017-08-15 08:10:01 -0500

    link mintic

--- app/common/categories/categoriesList/categoriesList.controller.js
+++ app/common/categories/categoriesList/categoriesList.controller.js
@@ -8,7 +8,7 @@ class CategoryListController {
   $onInit() {
     this.categories = [
       //{id:1, title: '¿Quieres postular tu producto o servicio?', img: 'assets/img/postular.png', stateUrl: 'registerEntity'},
-      {id:2, title: '¿Quieres ser parte de la comunidad evaluadora?', img: 'assets/img/comunidad.png', stateUrl: 'registerEvaluator'},
+      {id:2, title: '¿Quieres ser parte de la comunidad evaluadora?', img: 'assets/img/comunidad.png', stateUrl: 'https://goo.gl/forms/Ouzw6Ag0IfzLWOdg1'},
       //{id:3, title: 'Conoce y calífica los productos certificados', img: 'assets/img/evaluando.png', stateUrl: 'home'}
     ]
   }
