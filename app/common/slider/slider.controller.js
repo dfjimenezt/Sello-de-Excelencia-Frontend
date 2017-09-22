0782719d4ccb1f8b14f556c81ad9e82b33847929
Daniel Jiménez <daniel@domoti.rocks>
2017-09-22 07:43:27 -0500

    sliders

--- app/common/slider/slider.controller.js
+++ app/common/slider/slider.controller.js
@@ -22,9 +22,9 @@ class SliderController {
         </a>
       </li>`
     }
-    this.video_template = '<video style="background-color:#000;" src="{{VIDEO}}" poster="{{IMAGE}}" autoplay controls></video>'
+    this.video_template = '<video style="background-color:#000;" src="{{VIDEO}}" poster="{{IMAGE}}" controls></video>'
     this.image_template = '<img src="{{IMAGE}}" />'
-    this.caption_template = '<div class="_caption"><h5 class="color-white font-bold">{{TITLE}}</h5><p class="color-white summary">{{SUMMARY}}</p>{{MORE}}</div>'
+    this.caption_template = '<div class="_caption"><div class="container"><h5 class="color-white font-bold">{{TITLE}}</h5><p class="color-white summary">{{SUMMARY}}</p>{{MORE}}</div></div>'
     this.more_template = '<a class="btn lightseagreen" href="#!/banner/{{ID}}">VER MÁS<a>'
   }
   $onInit() {
