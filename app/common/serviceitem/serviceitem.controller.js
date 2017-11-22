692f4e460f53cff484798449830febecf6fe1064
Daniel Jiménez <daniel@domoti.rocks>
2017-11-22 12:24:08 -0500

    status

--- app/common/serviceitem/serviceitem.controller.js
+++ app/common/serviceitem/serviceitem.controller.js
@@ -4,16 +4,18 @@ class serviceItemController{
     this.Api = Api
     this.STATES = STATES
     this.$http = $http
-    this.serviceEndpoint = Api+'/service/service_status?limit=1&order=timestamp%20desc&filter_field=id_service&filter_value='
+    this.serviceEndpoint = Api+'/service/service_status?&order=timestamp%20desc&filter_field=id_service&filter_value='
   }
   $onInit(){
     this.$http.get(this.serviceEndpoint+this.item.id).then((results)=>{
-      this.item.status = results.data.data[0]
-      if(this.item.status.timestamp.indexOf('T') >-1 ){
-        this.item.status.timestamp = this.item.status.timestamp.split('T')[0]
-      }
-      if(this.item.status.valid_to.indexOf('T') >-1 ){
-        this.item.status.valid_to = this.item.status.valid_to.split('T')[0]
+      if(this.item.certified){
+        results.data.data.forEach((status)=>{
+          if(status.id_status === this.STATES.SERVICE.CUMPLE){
+            this.item.status = status
+          }
+        })
+      }else{
+        this.item.status = results.data.data[0]
       }
       if(this.item.url.indexOf('http') !== 0){
         this.item.url = 'http://'+this.item.url
