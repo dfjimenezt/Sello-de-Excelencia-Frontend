93378717a89eb5774429d3e9fd37989a764ca085
Daniel Jiménez <daniel@domoti.rocks>
2017-11-24 15:17:25 -0500

    embed

--- app/common/embed/embed.controller.js
+++ app/common/embed/embed.controller.js
@@ -4,6 +4,7 @@ class EmbedController {
     this.$state = $state
     this.Api = Api
     this.$http = $http
+    this.detailEndpoint = Api+'/service/service?id='+$state.params.id
     if($auth.isAuthenticated()){
       this.disabled = $auth.getPayload().role === 'Evaluador' ||
         $auth.getPayload().role === 'Entidad'
@@ -12,7 +13,7 @@ class EmbedController {
     }
   }
   $onInit() {
-    
+    this.getData()
   }
   getData(){
     this.$http.get(this.detailEndpoint).then((results)=>{
